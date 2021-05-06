c89459822ead35a4b53ad2b546f930467325950c
정성용 <puppy3987@gmail.com>
2021-05-06 13:47:05 +0900

    getter setter

--- JavaEx/src/com/javaex/oop/goods/v2/Goods.java
+++ JavaEx/src/com/javaex/oop/goods/v2/Goods.java
@@ -1,9 +1,30 @@
 package com.javaex.oop.goods.v2;
 
-//	v2.	정보 은
+//	v2.	정보 은닉
+//		정보에 접근할 수 있는 특수 메서드 : getter / setter 설
 public class Goods {
 	//	필드(default: 내부 + 동일 패키)
 	private String name;
 	private int price;
-
+	
+	// 	Getter/Setter
+	public String getName() {
+		return name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	//	메서드
+	public void showInfo() {
+		System.out.println("상품명:" + name);
+		System.out.println("가격:" + price);
+	}
 }
--- JavaEx/src/com/javaex/oop/goods/v2/GoodsApp.java
+++ JavaEx/src/com/javaex/oop/goods/v2/GoodsApp.java
@@ -4,19 +4,26 @@ public class GoodsApp {
 
 	public static void main(String[] args) {
 		Goods notebook = new Goods();
-		notebook.name = "LG gram";
-		notebook.price = 1500000;
+		//	setter를 이용한 우회 설
+//		notebook.name = "LG gram";
+//		notebook.price = 1500000;
+		notebook.setName("LG Gram");
+		notebook.setPrice(1500000);	
 		
 		Goods smartphone = new Goods();
-		smartphone.name = "Iphone";
-		smartphone.price = 1000000;
+		smartphone.setName("Iphone");
+		smartphone.setPrice(1000000);
 		
 		System.out.printf("%s, %d원%n",
-				notebook.name, 
-				notebook.price);
+				notebook.getName(), 
+				notebook.getPrice());
 		System.out.printf("%s, %d원%n",
-				smartphone.name, 
-				smartphone.price);
+				smartphone.getName(), 
+				smartphone.getPrice());
+		
+		//	내장 메서드 호출
+		notebook.showInfo();
+		smartphone.showInfo();
 	}
 
 }
